bde1ef02460be1359fe013f795f891042d3642de
omutucat <y.kuma1102@gmail.com>
2024-09-23 00:36:24 +0900

    ゲーム全体を通して生存するSystemManagerを追加

---
+++ Assets/Scripts/SystemManager.cs
@@ -0,0 +1,25 @@
+using System;
+using NoMoney.Assets.Scripts.Board;
+using UnityEngine;
+
+namespace NoMoney.Assets.Scripts
+{
+    public class SystemManager : MonoBehaviour
+    {
+        public static StageName CurrentStage { get; set; } = StageName.Stage1;
+        public static SystemManager? Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
